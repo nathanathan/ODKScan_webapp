c01fb5fa3220e1c7d8bf1c790881d7ca1f0d259f
Nathan Breit <nabreit@gmail.com>
2013-04-11 13:35:30 -0400

    Trying to add textile xlsform docs

--- templatetags/scantags.py
+++ templatetags/scantags.py
@@ -1,5 +1,6 @@
 from ODKScan_webapp.models import FormImage
 from django import template
+import textile
 
 register = template.Library()
 
@@ -13,3 +14,16 @@ def display_markedup_image(formImageId):
     #print >>sys.stderr, dir(form_image.image)
     markedup_image_url = form_image.image.url.partition("photo/")[0] + "markedup.jpg"
     return '<img src="%s"></img>' % markedup_image_url
+    
+@register.tag('begin_textile')
+def do_upper(parser, token):
+    nodelist = parser.parse(('end_textile',))
+    parser.delete_first_token()
+    return UpperNode(nodelist)
+
+class UpperNode(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+    def render(self, context):
+        output = self.nodelist.render(context)
+        return textile.textile(output)
