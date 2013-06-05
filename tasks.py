b798727a0c4b1964eb74b6a37a64842c1d9ab569
Nathan Breit <nabreit@gmail.com>
2013-06-04 18:39:39 -0600

    Switching over to Scan's JSON API

--- tasks.py
+++ tasks.py
@@ -1,32 +1,65 @@
 import os
 import sys, subprocess
 APP_ROOT = os.path.dirname(__file__)
-
 from ODKScan_webapp.models import FormImage
-
 import celery
+import json
 
 @celery.task
 def process_image(id):
+    """
+    Process the form image with the given id.
+    """
     obj = FormImage.objects.get(id=id)
+    #w for working, because p for processing is already taken.
     obj.status = 'w'
     obj.save()
     stdoutdata, stderrdata = subprocess.Popen(['./ODKScan.run',
-                      os.path.dirname(obj.template.image.path) + '/',
-                      obj.image.path,
-                      obj.output_path
-                      ],
+        json.dumps({
+            "inputImage" : obj.image.path,
+        	"outputDirectory" : obj.output_path,
+        	"alignForm" : True,
+        	"processForm" : True,
+            "detectOrientation" : True,
+        	"templatePath" : os.path.dirname(obj.template.image.path) + '/',
+        	"trainingDataDirectory" : "training_examples/"
+        })],
         cwd=os.path.join(APP_ROOT,
                          'ODKScan-core'),
-        #env={'LD_LIBRARY_PATH':'/usr/local/lib'}, #TODO: This could cause problems on other systems, document or fix
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT).communicate()
-    print >>sys.stdout, stdoutdata
+    # I'm in trouble if this string ever ends up in the JSON result or processing log.
+    log, result_text = stdoutdata.split("<======= RESULT =======>")
     obj.processing_log = stdoutdata
-    json_path = os.path.join(obj.output_path, 'output.json')
-    if os.path.exists(json_path):
-        #process_json_output(json_path)#Not sure I need this.
-        obj.status = 'p'
-    else:
+    result = json.loads(result_text)
+    if "errorMessage" in result:
         obj.status = 'e'
+    else:
+        obj.status = 'p'
     obj.save()
+
+#@celery.task
+#def process_image(id):
+#    obj = FormImage.objects.get(id=id)
+#    obj.status = 'w'
+#    obj.save()
+#    stdoutdata, stderrdata = subprocess.Popen(['./ODKScan.run',
+#                      os.path.dirname(obj.template.image.path) + '/',
+#                      obj.image.path,
+#                      obj.output_path
+#                      ],
+#        cwd=os.path.join(APP_ROOT,
+#                         'ODKScan-core'),
+#        #env={'LD_LIBRARY_PATH':'/usr/local/lib'}, #TODO: This could cause problems on other systems, document or fix
+#        stdout=subprocess.PIPE,
+#        stderr=subprocess.STDOUT).communicate()
+#    print >>sys.stdout, stdoutdata
+#    obj.processing_log = stdoutdata
+#    json_path = os.path.join(obj.output_path, 'output.json')
+#    if os.path.exists(json_path):
+#        #process_json_output(json_path)#Not sure I need this.
+#        obj.status = 'p'
+#    else:
+#        obj.status = 'e'
+#    obj.save()
+    
